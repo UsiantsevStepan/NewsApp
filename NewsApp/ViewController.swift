040862fd0f38d75364fada7bda864ef6f9c3e9d7
Usiantsev Stepan <usiantsevstepan@gmail.com>
2020-07-06 14:13:20 +0500

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Степан Усьянцев on 06.07.2020.
+//  Copyright © 2020 Stepan Usiantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
